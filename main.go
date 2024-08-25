92cd782d4040f432eaa850dec2c4efd54a93be1f
PedroPBSilva <ppbastosdasilva@gmail.com>
2024-08-25 00:52:32 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/PedroPBSilva/gopportunities.git/router"
+import (
+	"github.com/PedroPBSilva/gopportunities.git/config"
+	"github.com/PedroPBSilva/gopportunities.git/router"
+)
 
+var (
+	logger *config.Logger
+)
 func main() {
+	//Instance Logger
+	logger = config.GetLogger("main")
+	
+	//Inicialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config inicialization DB error: %v", err)
+		return
+	}
+
 	//Inicialize router
 	router.Inicialize()
 }
