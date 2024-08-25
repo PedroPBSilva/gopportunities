29ad699d7ea87a711f5ee6b56a7e062daf8f1324
PedroPBSilva <ppbastosdasilva@gmail.com>
2024-08-25 17:54:08 -0300

    feat: add createOpening handler

--- handler/createopenig.go
+++ handler/createopenig.go
@@ -3,13 +3,36 @@ package handler
 import (
 	"net/http"
 
+	"github.com/PedroPBSilva/gopportunities.git/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateOpeningHandler(context *gin.Context) {
 
-	
-	context.JSON(http.StatusOK, gin.H{
-		"message": "Handler POST opening",
-	})
+	request := CreateOpeningRequest{}
+
+	context.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("Validation error: %v", err.Error())
+		sendError(context, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opening := schemas.Opening{
+		Role: request.Role,
+		Company: request.Company,
+		Location: request.Location,
+		Remote: *request.Remote,
+		Link: request.Link,
+		Salary: request.Salary,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
+		logger.Errorf("Error creating opening: %v", err.Error())
+		sendError(context, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	sendSucess(context, "create-opening", opening)
 }
--- schemas/opening.go
+++ schemas/opening.go
@@ -1,8 +1,6 @@
 package schemas
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
   
@@ -16,15 +14,15 @@ type Opening struct {
 	Salary int64
 }
 
-type OpeningResponse struct {
-	ID uint `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	DeletedAt time.Time `json:"deleted_at,omitempty"`
-	Role  string `json:"role"`
-	Company string `json:"company"`
-	Location string `json:"location"`
-	Remote bool `json:"remote"`
-	Link string `json:"link"`
-	Salary int64 `json:"salary"`
-}
+// type OpeningResponse struct {
+// 	ID uint `json:"id"`
+// 	CreatedAt time.Time `json:"created_at"`
+// 	UpdatedAt time.Time `json:"updated_at"`
+// 	DeletedAt time.Time `json:"deleted_at,omitempty"`
+// 	Role  string `json:"role"`
+// 	Company string `json:"company"`
+// 	Location string `json:"location"`
+// 	Remote bool `json:"remote"`
+// 	Link string `json:"link"`
+// 	Salary int64 `json:"salary"`
+// }
